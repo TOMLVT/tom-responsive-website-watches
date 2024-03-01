80e341947ba6cb61778e634c1333e65de28e87af
TOMLVT <luuvythuan1204@gmail.com>
2024-03-01 19:22:18 +0700

    code

--- assets/js/main.js
+++ assets/js/main.js
@@ -146,3 +146,19 @@ themeButton.addEventListener('click', () => {
     localStorage.setItem('selected-theme', getCurrentTheme())
     localStorage.setItem('selected-icon', getCurrentIcon())
 })
+
+
+//vo hieu hoa code
+document.addEventListener("keydown", function (event){
+    if (event.ctrlKey){
+       event.preventDefault();
+    }
+    if(event.keyCode == 123){
+       event.preventDefault();
+    }
+  });
+  
+  document.addEventListener('contextmenu', function(event) {
+    event.preventDefault(); // Chặn hành động mặc định khi chuột phải được nhấp
+    alert('Vĩ Thuận đã chặn xem code!');
+  });
